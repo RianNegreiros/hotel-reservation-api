872c522d4d6a30eec1fd9df037b5d1f80f7f2a69
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-05 16:00:01 -0300

    sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,26 @@ import (
 	"go-lodging/pkg/render"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.19
 require github.com/go-chi/chi/v5 v5.0.8
 
 require github.com/justinas/nosurf v1.1.1
+
+require github.com/alexedwards/scs/v2 v2.5.0
