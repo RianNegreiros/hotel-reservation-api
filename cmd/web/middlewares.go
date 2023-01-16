e57e1e4755b39ea787f724e54992e2a8f9bef303
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-16 16:05:49 -0300

    authenticate user

--- cmd/web/middlewares.go
+++ cmd/web/middlewares.go
@@ -6,6 +6,7 @@ import (
 	"github.com/justinas/nosurf"
 )
 
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -19,6 +20,18 @@ func NoSurf(next http.Handler) http.Handler {
 	return csrfHandler
 }
 
+// SessionLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+// Auth middleware to check if user is authenticated
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.Session.Exists(r.Context(), "user_id") {
+			http.Redirect(w, r, "/user/login", 302)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -28,8 +28,17 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/make-reservation", handlers.Repo.PostReservation)
 	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
+	mux.Get("/user/login", handlers.Repo.ShowLogin)
+	mux.Post("/user/login", handlers.Repo.PostShowLogin)
+	mux.Get("/user/logout", handlers.Repo.Logout)
+
 	filesServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", filesServer))
+
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(Auth)
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+	})
 
 	return mux
 }
