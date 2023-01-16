1851e71f898d123b6a6dbde2140c8731fa9cb738
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-01-16 15:31:56 -0300

    database

---
+++ internal/repository/dbrepo/dbrepo.go
@@ -0,0 +1,19 @@
+package dbrepo
+
+import (
+	"database/sql"
+	"go-lodging/internal/config"
+	"go-lodging/internal/repository"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
