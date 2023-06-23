d819c29a41afa47390f467658ce890f765f8a676
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-23 14:47:18 -0300

    TestPostUser

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/RianNegreiros/hotel-reservation/db"
+	"github.com/RianNegreiros/hotel-reservation/types"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -38,4 +43,38 @@ func setup(t *testing.T) *testdb {
 func TestPostUser(t *testing.T) {
 	db := setup(t)
 	defer db.teardown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(db.UserStore)
+	app.Post("/", userHandler.HandlePostUser)
+
+	params := types.CreateUserParams{
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "johndoe@mail.com",
+		Password:  "123456",
+	}
+
+	b, _ := json.Marshal(params)
+
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	var user types.User
+	json.NewDecoder(resp.Body).Decode(&user)
+
+	if user.FirstName != params.FirstName {
+		t.Errorf("Expected %s, got %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.LastName {
+		t.Errorf("Expected %s, got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("Expected %s, got %s", params.Email, user.Email)
+	}
 }
