1768cab8e800d8c24a617badb040ce3f788f0b4d
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-25 14:51:57 -0300

    add hotel and room store

---
+++ db/hotel_store.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"context"
+
+	"github.com/RianNegreiros/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type HotelStore interface {
+	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Update(context.Context, bson.M, bson.M) error
+}
+
+type MongoHotelStore struct {
+	client     *mongo.Client
+	collection *mongo.Collection
+}
+
+func NewMongoHotelStore(client *mongo.Client) *MongoHotelStore {
+	return &MongoHotelStore{
+		client:     client,
+		collection: client.Database(DBNAME).Collection("hotels"),
+	}
+}
+
+func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
+	resp, err := s.collection.InsertOne(ctx, hotel)
+	if err != nil {
+		return nil, err
+	}
+	hotel.ID = resp.InsertedID.(primitive.ObjectID)
+	return hotel, nil
+}
+
+func (s *MongoHotelStore) Update(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := s.collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ db/room_store.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"context"
+
+	"github.com/RianNegreiros/hotel-reservation/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RoomStore interface {
+	InsertRoom(context.Context, *types.Room) (*types.Room, error)
+}
+
+type MongoRoomStore struct {
+	client     *mongo.Client
+	collection *mongo.Collection
+
+	HotelStore
+}
+
+func NewMongoRoomStore(client *mongo.Client, hotelStore HotelStore) *MongoRoomStore {
+	return &MongoRoomStore{
+		client:     client,
+		collection: client.Database(DBNAME).Collection("rooms"),
+		HotelStore: hotelStore,
+	}
+}
+
+func (s *MongoRoomStore) InsertRoom(ctx context.Context, room *types.Room) (*types.Room, error) {
+	resp, err := s.collection.InsertOne(ctx, room)
+	if err != nil {
+		return nil, err
+	}
+	room.ID = resp.InsertedID.(primitive.ObjectID)
+
+	filter := bson.M{"_id": room.HotelID}
+	update := bson.M{"$push": bson.M{"rooms": room.ID}}
+	if err := s.HotelStore.Update(ctx, filter, update); err != nil {
+		return nil, err
+	}
+
+	return room, nil
+}
