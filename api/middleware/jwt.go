d089e060ed7a0d334883050cfaae52ab5f033b1f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-06-27 13:05:32 -0300

    add JWT authentication middleware

---
+++ api/middleware/jwt.go
@@ -0,0 +1,63 @@
+package middleware
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/RianNegreiros/hotel-reservation/db"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTAuthentication(userStore db.UserStore) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		token, ok := c.GetReqHeaders()["X-Api-Token"]
+		if !ok {
+			fmt.Println("token not present in the header")
+			return fiber.ErrUnauthorized
+		}
+		claims, err := validateToken(token)
+		if err != nil {
+			return err
+		}
+
+		expiresFloat := claims["expires"].(float64)
+		expires := int64(expiresFloat)
+		if time.Now().Unix() > expires {
+			return fiber.ErrUnauthorized
+		}
+		userID := claims["id"].(string)
+		user, err := userStore.GetUserByID(c.Context(), userID)
+		if err != nil {
+			return fiber.ErrUnauthorized
+		}
+		c.Context().SetUserValue("user", user)
+
+		return c.Next()
+	}
+}
+
+func validateToken(tokenStr string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("invalid signing method", token.Header["alg"])
+			return nil, fiber.ErrUnauthorized
+		}
+		secret := os.Getenv("JWT_SECRET")
+		return []byte(secret), nil
+	})
+	if err != nil {
+		fmt.Println("failed to parse JWT token:", err)
+		return nil, fiber.ErrUnauthorized
+	}
+	if !token.Valid {
+		fmt.Println("invalid token")
+		return nil, fiber.ErrUnauthorized
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fiber.ErrUnauthorized
+	}
+	return claims, nil
+}
